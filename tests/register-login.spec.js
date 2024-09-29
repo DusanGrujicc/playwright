8df62fae7a82e060326b1dd81e33bd9e715e6dfa
Dusan <dusangrujic97@gmail.com>
2024-09-29 16:49:13 +0200

    beforeAll hook added

--- tests/register-login.spec.js
+++ tests/register-login.spec.js
@@ -1,18 +1,29 @@
 const { test, page, expect } = require("@playwright/test");
 
-test("registe and login", async ({ page }) => {
-  await page.goto("https://automaticityacademy.ngrok.app/register");
-  await page.getByRole("textbox", { name: "username" }).fill("dusan123");
-  await page.getByRole("textbox", { name: "email" }).fill("dusan123@gmail.com");
-  await page.getByRole("textbox", { name: "password" }).fill("test1234");
-  await page.click("button:has-text('Register')");
+test.describe("register test", async () => {
+  test.beforeEach("visit app", async ({ page }) => {
+    await page.goto("https://automaticityacademy.ngrok.app");
+  });
 
-  //   await expect(page.getByText("successfully registered!")).toBeVisible();
-});
+  test("registe and login", async ({ page }) => {
+    await page.click("button:has-text('Sign up')");
 
-test("login", async ({ page }) => {
-  await page.goto("https://automaticityacademy.ngrok.app/login");
-  await page.getByRole("textbox", { name: "email" }).fill("dusan123@gmail.com");
-  await page.getByRole("textbox", { name: "password" }).fill("test1234");
-  await page.click("button:has-text('Sign in')");
+    await page.getByRole("textbox", { name: "username" }).fill("dusan123");
+    await page
+      .getByRole("textbox", { name: "email" })
+      .fill("dusan123@gmail.com");
+    await page.getByRole("textbox", { name: "password" }).fill("test1234");
+    await page.click("button:has-text('Register')");
+  });
+
+  test("login", async ({ page }) => {
+    await page.click("button:has-text('Sign up')");
+    await page.click("a:has-text('Log in now!')");
+
+    await page
+      .getByRole("textbox", { name: "email" })
+      .fill("dusan123@gmail.com");
+    await page.getByRole("textbox", { name: "password" }).fill("test1234");
+    await page.click("button:has-text('Sign in')");
+  });
 });
