0cf559d94606a4dd1c06d87131c16bce196c7217
Dusan Grujic <dusangrujic97@gmail.com>
2024-10-03 22:03:21 +0200

    register page change

--- tests/register.spec.js
+++ tests/register.spec.js
@@ -1,11 +1,13 @@
 import { test, expect } from "@playwright/test";
 import { RegisterPage } from "../pom/modules/ui/registerPage";
 import { describe } from "node:test";
-
+let registerPage
 test.describe("Register",()=>{
     test.beforeEach('visit app and instantiate POM',async({page})=>{
         await page.goto('/register')
         registerPage= new RegisterPage(page)
     })
-    
+    test("Log in link in viewpoin", async({page})=>{
+        await expect(registerPage.link)
+    })
 })
