47a23a50436d290408b93abf8a146d86f8116622
Dusan <dusangrujic97@gmail.com>
2024-10-17 14:04:51 +0200

    small changes

--- tests/authApi.spec.js
+++
@@ -1,4 +0,0 @@
-import { test, expect } from "@playwright/test";
-import { generateUserCredentials, HEADING, URLS, utils } from "../fixtures";
-import { LoginPage } from "../pom/modules/ui/loginPage";
-import { RegisterPage } from "../pom/modules/ui/registerPage";
--- tests/ui/basic-auth.spec.js
+++ tests/ui/basic-auth.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
-import { generateUserCredentials, HEADING, URLS } from "../fixtures";
-import { LoginPage } from "../pom/modules/ui/loginPage";
-import { RegisterPage } from "../pom/modules/ui/registerPage";
+import { generateUserCredentials, HEADING, URLS } from "../../fixtures";
+import { LoginPage } from "../../pom/modules/ui/loginPage";
+import { RegisterPage } from "../../pom/modules/ui/registerPage";
 
 let loginEmail, loginPassword;
 test.describe.configure({ mode: "serial" });
